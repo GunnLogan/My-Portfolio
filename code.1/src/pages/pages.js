e986f85cd27898ce5fb1cadc1a86312aaaafce02
Gunnlaugur Egilsson <gunnlaugur.egilsson@hyperisland.se>
2018-10-11 10:09:02 +0200

    in the making

---
+++ code.1/src/pages/pages.js
@@ -0,0 +1,15 @@
+import React from "react"
+import LandingPage from "./landing-page/landingPage"
+import ProjectPages from "./project-pages/projectPages"
+class Pages extends React.Component {
+
+  render() {
+    return (
+      <div className="greeting">
+      </div>
+    )
+  }
+
+}
+
+export default Pages
---
+++ code.1/src/pages/project-pages/projectPages.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./projectPages.css"
+class ProjectPages extends React.Component {
+
+  render() {
+    return (
+      <div className="project1">
+        hello again
+      </div>
+    )
+  }
+
+}
+
+export default ProjectPages
