b21d62455d3c9a905106bb02ddeadd4cbfa461b8
Gunnlaugur Egilsson <gunnlaugur.egilsson@hyperisland.se>
2018-10-25 13:01:32 +0200

    changes

--- src/components/TheaterAndDance/theaterAndDance.js
+++ src/components/TheaterAndDance/theaterAndDance.js
@@ -1,32 +1,32 @@
 import React from "react"
 import {Link} from "react-router-dom"
-import "./theaterAndDance.css"
+import "../Tech/tech.css"
 
 
-const theAndDancasesJson = require("../../theAndDancases.json")
+const casesJson = require("../../cases.json")
 
 
 class TheaterAndDance extends React.Component {
 
     render() {
       return (
-        <div className="theaterAndDanceContainer">
-            <div nameClass="theaterAndDanceheader">
-            <h4>Theater & Dance</h4>
+        <div className="techContainer">
+            <div nameClass="techheader">
+            <h4>THEATER & DANCE</h4>
           </div>
-          <div className="theaterAndDanceText">
+          <div className="techText">
             <p>20 years of experience working as a professional dancer with The Royal Swedish Ballet, Geneva Ballet, Icelandic Dance Company and National Ballet of Canada.
                Choreographies for The Royal Opera House in Stockholm, Icelandic Dance Company, Orion Theater and the Royal Dramatic Theater </p>
           </div>
-          <div className = "alltheAndDanCases">
-            <div className = "theAndDancasesContainer">
-            {theAndDanCaseJson.cases.map((item) => {
-                return (
-                  <div className = "theAndDancase" >
-                    <Link to ={`/cases/${item.id}`}>
-                      <img src={item.image}/>
-                    </Link ><p>{item.description}</p>
-                  </div>)
+          <div className = "allCases">
+            <div className = "casesContainer">
+            {casesJson.cases.map((item) => {
+              return (
+                <div className = "case" >
+                  <Link to ={`/danceCases/${item.id}`}>
+                    <img src={item.image}/>
+                  </Link ><p>{item.description}</p>
+                </div>)
                 })
             }
 
