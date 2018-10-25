154da3920d3c979331b311a9f7262a55cd217fcf
Gunnlaugur Egilsson <gunnlaugur.egilsson@hyperisland.se>
2018-10-25 12:11:49 +0200

    images and links to Radio

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import React from "react"
 import { HashRouter as Router, Route, Link,} from "react-router-dom"
 import LandingPage from "./pages/landing-page/landingPage"
 import Cases from "./pages/cases/cases"
+
+
 import Button from "./components/button/button"
 class App extends React.Component {
 
@@ -11,7 +13,7 @@ class App extends React.Component {
         <div>
           <Route path= "/" exact component = {LandingPage}/>
           <Route path= "/cases/:id" component = {Cases}/>
-          <Route path= "/theAndDancases/:id" component = {theAndDanCases}/>
+          <Route path= "/theateranddancecases/:id" component = {Cases}/>
 
         </div>
       </Router>
